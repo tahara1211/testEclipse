239ca72b3ecd3f310e6c07c801d070e188cfcc2a
tahara1211 <taharatsukuru@gmail.com>
2024-07-09 13:20:58 +0900

    新規プロジェクト作成あああああああああああ

---
+++ TestGit/src/test/GitTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GitTest {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		System.out.println("Hello GitHub?!");
+		
+		
+	}
+}
